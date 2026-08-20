e4bf0ea8ef647f23d5179eccdfa8bc3002f8fd4d
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-20 16:26:56 +0200

    FIX: Update official repository URL in header files and add compile flags

--- BootChk/Config.h
+++ BootChk/Config.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2024-2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
-// Official repository: https://github.com/ne-app-eu/krnl
+// Official repository: https://github.com/ne-app/snippet-efi-driver-bootchk
 
 #ifndef NEKIT_CONFIG_H
 #define NEKIT_CONFIG_H
